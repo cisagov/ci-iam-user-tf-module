50fe3adac44d1006290b76345f12e6de86f6d772
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-04 10:23:33 -0500

    Only create a single CI role, instead of two

--- role.tf
+++ role.tf
@@ -1,21 +1,13 @@
-# The production role
-resource "aws_iam_role" "production" {
-  provider = aws.production
+# The CI role
+resource "aws_iam_role" "ci" {
+  provider = aws.ci
 
   assume_role_policy   = data.aws_iam_policy_document.assume_role_doc.json
   description          = var.role_description
   max_session_duration = var.role_max_session_duration
-  name                 = join("-", [var.role_name, "Production"])
-  tags                 = var.production_role_tags
-}
-
-# The staging role
-resource "aws_iam_role" "staging" {
-  provider = aws.staging
-
-  assume_role_policy   = data.aws_iam_policy_document.assume_role_doc.json
-  description          = var.role_description
-  max_session_duration = var.role_max_session_duration
-  name                 = join("-", [var.role_name, "Staging"])
-  tags                 = var.staging_role_tags
+  name                 = var.role_name
+  tags = {
+    "GitHub_Secret_Name"             = "TEST_ROLE_TO_ASSUME",
+    "GitHub_Secret_Terraform_Lookup" = "arn"
+  }
 }
