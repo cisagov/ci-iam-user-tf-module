3e59d2a83740dbf1f8dd94faee51f15f4b16de99
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-04 10:20:25 -0500

    Reduce to a single provider
    Now that the CI user operates in a single environment (e.g. Prod, Staging, or Dev), multiple providers are no longer needed.

--- providers.tf
+++ providers.tf
@@ -1,11 +1,4 @@
-# This is the provider that is used to create the CI role inside the
-# production account
+# This is the provider that is used to create the CI role
 provider "aws" {
-  alias = "production"
-}
-
-# This is the provider that is used to create the CI role inside the
-# staging account
-provider "aws" {
-  alias = "staging"
+  alias = "ci"
 }
